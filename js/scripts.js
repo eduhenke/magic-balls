0c96f05732f9b8492f8f2520e184ba0fe718c007
Eduardo Henke <eduardo_henke@hotmail.com>
2018-05-31 19:48:33 -0300

    Change CSS/JS for cell and table

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 window.onload = init;
 
 function init() {
-    var width = 9;
-    var height = 8;
+    var width = 5;
+    var height = 4;
     var container = document.getElementById("game");
     container.appendChild(createTable(width, height));
+    Cell();
+}
+
+function cellClickHandler(cell) {
+    return function() {
+        cell.addBall();
+    }
 }
 
 function createTable(width, height) {
@@ -14,8 +21,11 @@ function createTable(width, height) {
         var row = gameTable.insertRow(i);
 
         for (var j = 0; j < width; j++) {
-            row.insertCell(j).innerHTML = "X";
+            var cellElement = row.insertCell(j);
+            cell = new Cell(i, j, cellElement);
+            cellElement.onclick = cellClickHandler(cell);
         }
     }
     return gameTable;
 }
+
