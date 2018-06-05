2f952535c997f1675f290b65bfd228e7b3cbb1c0
kaduelinho <carlos.libardo@chaordicsystems.com>
2018-06-05 00:15:45 -0300

    Fix number of players

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ function init() {
     var height = 4;
     var container = document.getElementById("game");
     var buttonSubmit = document.getElementById("btnSubmit");
-    var buttonReset = document.getElementById("btnReset");
-    var nPlayers = document.getElementById("numberPlayers").value;
+    var buttonReset = document.getElementById("btnReset"); 
     buttonSubmit.onclick = function () {
+        var nPlayers = document.getElementById("numberPlayers").value;
         var game = new Game(nPlayers, width, height, container);
     }
     buttonReset.onclick = function () {
